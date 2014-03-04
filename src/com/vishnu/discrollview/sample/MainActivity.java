53aab1bed1549b8e06beb51675cf3dd898cbaa10
Vishnupriyan <vishnupriyan@inapp.com>
2014-03-04 13:59:50 +0530

    initial commit

---
+++ src/com/vishnu/discrollview/sample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vishnu.discrollview.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.vishnu.discrollview.R;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
